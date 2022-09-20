94da3b0848eaec2114df20b8426638784a5190c8
mikeson <mikeson.jnr@gmail.com>
2022-09-20 19:28:02 +0100

    Add functionality to addBookToLibrary

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let myLibrary = [];
 
-function Book() {
-  
+function Book(title) {
+  this.title = title;
 }
 
-function addBookToLibrary() {
-  
+Book.prototype.addBookToLibrary = function(){
+  myLibrary.push(this.title);
+  console.log(myLibrary);
 }
+
+const book1 = new Book("Harry Potter");
+const book2 = new Book("The Hunger Games");
+
+book1.addBookToLibrary()
+book2.addBookToLibrary()
