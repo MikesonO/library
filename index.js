a2f020dd66783a63e476e152cd3fa656aecb6947
mikeson <mikeson.jnr@gmail.com>
2022-09-21 19:24:05 +0100

    Update createCard function to display user input

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 const bookTitle = document.getElementById("title");
 const bookAuthor = document.getElementById("author");
 const bookPages = document.getElementById("pages");
-const add = document.getElementById("add");
+const add = document.getElementById("btn-add");
 const library = document.querySelector(".my-books");
 
+//Empty Library Array
 let myLibrary = [];
 
+//Book Constructor
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
-
-Book.prototype.addBookToLibrary = function(){
-  myLibrary.push(this.title);
-  myLibrary.push(this.author);
-  myLibrary.push(this.pages);
-  console.log(myLibrary);
-}
-
+//Add Book btn retrives User's Input
   add.addEventListener("click", ()=>{
     const book = new Book(`${bookTitle.value}`,`${bookAuthor.value}`,`${bookPages.value}`);
-    book.addBookToLibrary()
-    createCard();
+    myLibrary.push(book);
+    for (let i = 0; i < myLibrary.length; i++) {
+      console.log(myLibrary.length);
+      createCard(myLibrary[i]);
+    }
     reset();
   });
 
-  function createCard (){
+
+  //Create Card and Displays Book Information
+  function createCard (item){
     const card = document.createElement("div");
     card.className = "card";
     library.appendChild(card); 
+    
+    const pTitle = document.createElement('p');
+    const pAuthor = document.createElement('p');
+    const pPages = document.createElement('p');
+
+    pTitle.innerText = `${item.title}`
+    pAuthor.innerText = `Written by: ${item.author}`;
+    pPages.innerText = `Pages: ${item.pages}`;
+
+    card.appendChild(pTitle);
+    card.appendChild(pAuthor);
+    card.appendChild(pPages);
   }
 
   const reset = ()=>{
