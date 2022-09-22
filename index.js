0ff4f03fcfc8ab7558031d48871836ec72e01a6d
mikeson <mikeson.jnr@gmail.com>
2022-09-22 13:27:35 +0100

    Add readCount that increments when read is toggled

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const empty = document.querySelector(".empty-container");
 //Empty Library Array
 let myLibrary = [];
 
+let count = 0;
+
 //Book Constructor
 function Book(title, author, pages) {
   this.title = title;
@@ -15,74 +17,83 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 //Add Book btn retrives User's Input
-  add.addEventListener("click", ()=>{
-    const book = new Book(`${bookTitle.value}`,`${bookAuthor.value}`,`${bookPages.value}`);
-    myLibrary.push(book);
+add.addEventListener("click", () => {
+  const book = new Book(`${bookTitle.value}`, `${bookAuthor.value}`, `${bookPages.value}`);
+  myLibrary.push(book);
+  updateBooks();
+  reset();
+});
+
+function updateBooks() {
+  library.querySelectorAll('div').forEach(n => n.remove()); //Deletes every book
+  for (let i = 0; i < myLibrary.length; i++) { //Updates and Displys books
+    createCard(myLibrary[i]);
+  }
+  checkIfEmpty();
+}
+
+
+//Create Card and Displays Book Information
+function createCard(item) {
+  const card = document.createElement("div");
+  card.className = "card";
+  library.appendChild(card);
+
+  const pTitle = document.createElement("p");
+  const pAuthor = document.createElement("p");
+  const pPages = document.createElement("p");
+  const deleteBtn = document.createElement("i");
+  const readCheck = document.createElement("label");
+  const toggleBtn = document.createElement("input");
+
+  pTitle.innerText = `${item.title}`
+  pAuthor.innerText = `Written by: ${item.author}`;
+  pPages.innerText = `Pages: ${item.pages}`;
+
+  let deleteBtnClasses = ["delete", "fa-solid", "fa-trash"];
+  deleteBtn.classList.add(...deleteBtnClasses);
+
+  readCheck.innerText = "Read:";
+  readCheck.classList.add("checkbox");
+  toggleBtn.setAttribute("type", "checkbox");
+  readCheck.appendChild(toggleBtn);
+
+  card.appendChild(pTitle);
+  card.appendChild(pAuthor);
+  card.appendChild(pPages);
+  card.appendChild(deleteBtn);
+  card.appendChild(readCheck);
+
+  deleteBtn.addEventListener("click", () => {
+    myLibrary.splice(myLibrary.indexOf(item), 1);
+    console.log(myLibrary);
+    console.log(myLibrary.length);
     updateBooks();
-    reset();
+    count = 0;
   });
 
-  function updateBooks(){
-    library.querySelectorAll('div').forEach(n => n.remove()); //Deletes every book
-    for (let i = 0; i < myLibrary.length; i++) { //Updates and Displys books
-      createCard(myLibrary[i]);
-    }
-      checkIfEmpty();
-  }
-  
-
-  //Create Card and Displays Book Information
-  function createCard (item){
-    const card = document.createElement("div");
-    card.className = "card";
-    library.appendChild(card); 
-    
-    const pTitle = document.createElement("p");
-    const pAuthor = document.createElement("p");
-    const pPages = document.createElement("p");
-    const deleteBtn = document.createElement("i");
-    const readCheck = document.createElement("label");
-    const toggleBtn = document.createElement("input");
-  
-
-    let deleteBtnClasses = ["delete", "fa-solid", "fa-trash"];
-    deleteBtn.classList.add(...deleteBtnClasses);
-    
-    readCheck.innerText = "Read:";
-    readCheck.classList.add("checkbox");
-    toggleBtn.setAttribute("type", "checkbox");
-    readCheck.appendChild(toggleBtn);
-
-    pTitle.innerText = `${item.title}`
-    pAuthor.innerText = `Written by: ${item.author}`;
-    pPages.innerText = `Pages: ${item.pages}`;
-
-    card.appendChild(pTitle);
-    card.appendChild(pAuthor);
-    card.appendChild(pPages);
-    card.appendChild(deleteBtn);
-    card.appendChild(readCheck);
-
-    deleteBtn.addEventListener("click", () => {
-      myLibrary.splice(myLibrary.indexOf(item), 1);
-      console.log(myLibrary);
-      console.log(myLibrary.length);
-      updateBooks();
-    });
-  }
-
-  //Resets Input Fields
-  const reset = ()=>{
-    bookTitle.value = "";
-    bookAuthor.value = "";
-    bookPages.value = "";
-  }
-
-  //Checks Library is empty
-  const checkIfEmpty = () =>{
-    if (myLibrary.length != 0){
-      empty.classList.add("hide");
+  readCheck.addEventListener("click", () => {
+    if (toggleBtn.checked == true) {
+      count++;
     } else {
-      empty.classList.remove("hide");
+      count--;
     }
+  });
+}
+
+//Resets Input Fields
+const reset = () => {
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookPages.value = "";
+  count = 0;
+}
+
+//Checks Library is empty
+const checkIfEmpty = () => {
+  if (myLibrary.length != 0) {
+    empty.classList.add("hide");
+  } else {
+    empty.classList.remove("hide");
   }
+}
